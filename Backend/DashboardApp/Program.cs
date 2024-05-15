caa3ec74981ad1aef8cb970a166d28c0346d6a55
ChrisPDev <49097864+ChrisPDev@users.noreply.github.com>
2024-05-15 10:24:57 +0200

    API data to table
    Added a table and cleaned up files

--- Backend/DashboardApp/Program.cs
+++ Backend/DashboardApp/Program.cs
@@ -14,14 +14,6 @@ namespace DashboardApp
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
 
             app.UseStaticFiles();
